ecdab2e8383114e08f0b98e3c71a896eb2201c6f
mirai301 <Mirai301@users.noreply.github.com>
2024-02-05 10:29:48 +0900

    Update Kali Config

--- bin/init.sh
+++ bin/init.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env zsh
 
-BASE_DIR=$(cd "$(dirname "$0")" && cd .. && pwd)
+BASE_DIR=$(pwd)
 
 # CI 例外処理用
 set -e
 
 echo "### Create symlinks for dotfiles"
-ln -sf "$BASE_DIR"/.zshrc       ~/.zshrc
-ln -sf "$BASE_DIR"/.tmux.conf        ~/.tmux.conf
-ln -sf "$BASE_DIR"/.vimrc        ~/.vimrc
-ln -sf "$BASE_DIR"/.gitconfig   ~/.gitconfig
+ln -sf "$BASE_DIR"/../.zshrc       ~/.zshrc
+ln -sf "$BASE_DIR"/../.tmux.conf        ~/.tmux.conf
+ln -sf "$BASE_DIR"/../.vimrc        ~/.vimrc
+ln -sf "$BASE_DIR"/../.gitconfig   ~/.gitconfig
+ln -sf "$BASE_DIR"/ss	/usr/bin/ss
 
 echo -e "\n[Finish]"
